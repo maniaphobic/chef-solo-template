423b5c9e3738dee80d47f3fc3c666e3383481015
Ken Maupin <kenneth.maupin@workday.com>
2013-11-06 16:47:18 -0800

    Checkpoint config

---
+++ lib/utilities.sh
@@ -0,0 +1,103 @@
+#________________________________________
+#
+#
+
+#
+# boot
+#
+
+function bootstrap () {
+
+    #
+    #
+    #
+
+    
+
+}
+
+#
+# 
+#
+
+function add_repos () {
+
+    case $1 in
+
+	CentOS,5.8)
+
+	    rpm -ivh http://mirrors.rit.edu/fedora/epel/5/i386/epel-release-5-4.noarch.rpm
+
+	*)
+	    echo '???'
+
+    esac
+
+}
+
+#
+# 
+#
+
+function bootstrap () {
+
+    #
+    #
+    #
+
+    case `get_distro_name_version` in
+
+	CentOS,5.8)
+	    bootstrap_centos_58
+	    ;;
+
+	*)
+	    echo '???'
+
+    esac
+
+}
+
+#
+# 
+#
+
+function bootstrap_centos_58 () {
+
+    add_repos
+
+}
+
+#
+#
+#
+
+function get_distro_name_version () {
+
+    #
+    #
+    #
+
+    distro_name=
+    distro_version=
+
+    #
+    #
+    #
+
+    if test -f /etc/lsb-release; then
+
+	source /etc/lsb-release
+	distro_name=`echo $DISTRIB_ID`
+	distro_version=`echo $DISTRIB_RELEASE`
+
+    elif test -f /etc/redhat-release; then
+
+	distro_name=`cat /etc/redhat-release | cut -d' ' -f1`
+	distro_version=`cat /etc/redhat-release | cut -d' ' -f3`
+
+    fi
+
+    echo "${distro_name},${distro_version}"
+
+}
